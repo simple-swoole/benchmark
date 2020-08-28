ea7d7f854cdcffc1b0d8319da4654e83630de03c
sy-records <52o@qq52o.cn>
2020-08-28 17:49:31 +0800

    update

--- config/servers.php
+++ config/servers.php
@@ -18,9 +18,7 @@ return [
         'callbacks' => [
         ],
         'settings' => [
-            'worker_num'       => swoole_cpu_num() * 2,
-            'open_tcp_nodelay' => true,
-            'enable_reuse_port' => true,
+            'worker_num' => swoole_cpu_num() * 2,
             'enable_coroutine' => false,
             'only_simple_http' => true,
         ],
